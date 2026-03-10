67f2cfb7989b2c2f16665adb7985028e9466ebbb
imshubhamgiri <your-email@example.com>
2026-03-10 17:41:15 +0530

    feat:bottom navigation bar completed

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
 import Sidebar from "../components/Sidebar"
+import BottomNav from "../components/BottomNav"
 
 
 const Layout = () => {
   return (
     <div className="layout-container">
       <Sidebar/>
+      <BottomNav/>
       <div className="flex-1 overflow-y-scroll ">
       <Outlet/>
       </div>
